4b73ed6c1061d943d16eb64d1b3d77b953c959c1
cipss <150723010+cipss@users.noreply.github.com>
2026-09-16 11:08:20 +0200

    refactor: drive X3+ launcher from app registry

--- src/activities/x3plus/X3PlusLauncherActivity.h
+++ src/activities/x3plus/X3PlusLauncherActivity.h
@@ -1,7 +1,6 @@
 #pragma once
 
 #include <string>
-#include <vector>
 
 #include "activities/Activity.h"
 #include "util/ButtonNavigator.h"
@@ -13,10 +12,8 @@ class X3PlusLauncherActivity final : public Activity {
   bool showPlaceholder = false;
   std::string placeholderMessage;
 
-  static const std::vector<std::string>& appNames();
-
   void selectApp();
-  void showPlaceholderFor(const std::string& appName);
+  void showPlaceholderFor(const char* appName);
 
  public:
   explicit X3PlusLauncherActivity(GfxRenderer& renderer, MappedInputManager& mappedInput)
